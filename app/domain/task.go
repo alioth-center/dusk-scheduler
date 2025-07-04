1d37a9bcb9ae7c7584994c1a9671176a4ea92b94
酒植 <jiuzhi@alioth.center>
2025-07-04 14:05:15 +0800

    feat: update task domain

--- app/domain/task.go
+++ app/domain/task.go
@@ -1,17 +1,22 @@
 package domain
 
-import "time"
+import (
+	"encoding/json"
+	"time"
+)
 
 const TableNameTask = "task"
 
 type Task struct {
-	ID        uint64       `gorm:"column:id;type:bigint(20);autoIncrement:true;primaryKey;not null"`
-	Submitter uint64       `gorm:"column:submitter;type:bigint(20);not null;default:0"`
-	Width     uint         `gorm:"column:width;type:int(10);not null;default:0"`
-	Height    uint         `gorm:"column:height;type:int(10);not null;default:0"`
-	Type      TaskType     `gorm:"column:type;type:tinyint(1);not null;default:0"`
-	Priority  TaskPriority `gorm:"column:priority;type:tinyint(1);not null;default:0"`
-
+	ID            uint64            `gorm:"column:id;type:bigint(20);autoIncrement:true;primaryKey;not null"`
+	Submitter     uint64            `gorm:"column:submitter;type:bigint(20);not null;default:0;index:idx_submitter"`
+	ContentHash   string            `gorm:"column:content_hash;type:varchar(64);not null;default:'';index:idx_content"`
+	Width         uint              `gorm:"column:width;type:int(10);not null;default:0"`
+	Height        uint              `gorm:"column:height;type:int(10);not null;default:0"`
+	Type          TaskType          `gorm:"column:type;type:tinyint(1);not null;default:0"`
+	Priority      TaskPriority      `gorm:"column:priority;type:tinyint(1);not null;default:0"`
+	Format        TaskFormat        `gorm:"column:format;type:tinyint(1);not null;default:0"`
+	ExtraOptions  json.RawMessage   `gorm:"column:extra_options;type:json;default:'{}'"`
 	CreatedAt     time.Time         `gorm:"column:created_at;type:timestamp;not null;default:CURRENT_TIMESTAMP"`
 	ScheduledAt   time.Time         `gorm:"column:scheduled_at;type:timestamp;not null"`
 	CompletedAt   time.Time         `gorm:"column:completed_at;type:timestamp;not null"`
